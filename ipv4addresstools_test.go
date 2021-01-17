84060796dc8b39844e050c1345f429360e0fae58
Benjamin Trachtenberg <btr1975@gmail.com>
2021-01-17 00:23:00 -0600

    started tests

---
+++ ipv4addresstools_test.go
@@ -0,0 +1,30 @@
+package nsojsonrpcrequestergo
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestIpV4Address(t *testing.T) {
+	scenarios := [] struct{
+		input string
+		expect string
+		rcvError error
+	}{
+		{input: "192.168.1.1", expect: "192.168.1.1", rcvError: nil},
+		{input: "192.168.1.0501", expect: "192.168.1.0501", rcvError: errors.New("not a valid IPv4 address")},
+	}
+
+	for _, scenario := range scenarios {
+		value, err := IpV4Address(scenario.input)
+		if value != scenario.expect {
+			t.Errorf("expected %v got %v", scenario.expect, value)
+		}
+		if err != scenario.rcvError {
+			t.Errorf("expected error %v got %v", scenario.rcvError, err)
+		}
+
+	}
+
+
+}
