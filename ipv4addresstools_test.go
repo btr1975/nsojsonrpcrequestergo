eb7dd1908106298102cef19a063a1f4199fe9d00
Benjamin Trachtenberg <btr1975@gmail.com>
2021-01-18 15:58:57 -0600

    added test

--- ipv4addresstools_test.go
+++ ipv4addresstools_test.go
@@ -20,11 +20,33 @@ func TestIpV4Address(t *testing.T) {
 		if value != scenario.expect {
 			t.Errorf("expected %v got %v", scenario.expect, value)
 		}
+
 		if err != scenario.rcvError {
-			t.Errorf("expected error %v got %v", scenario.rcvError, err)
+			if err.Error() != scenario.rcvError.Error() {
+				t.Errorf("expected error %v got %v", scenario.rcvError, err)
+			}
+
 		}
 
 	}
 
+}
 
+func TestIpV4MulticastAddress(t *testing.T) {
+	scenarios := [] struct{
+		input string
+		expect string
+		rcvError error
+	}{
+		{input: "192.168.1.1", expect: "192.168.1.1", rcvError: nil},
+		{input: "192.168.1.0501", expect: "192.168.1.0501", rcvError: errors.New("not a valid IPv4 address")},
+	}
+
+	for _, scenario := range scenarios {
+		value, _ := IpV4MulticastAddress(scenario.input)
+		if value != scenario.expect {
+			t.Errorf("expected %v got %v", scenario.expect, value)
+		}
+
+	}
 }
